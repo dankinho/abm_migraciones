c8d4eda1e5139a56b72b0536a973048b6e697be8
dankinho <dani_1453@hotmail.com>
2018-10-25 07:58:50 -0400

    creacion de abm y migraciones

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.plantilla')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Iniciar Sesion') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('inicio') }}" aria-label="{{ __('Login') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="text" class="form-control" name="nombre_usuario" value="" required autofocus>
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Ingresar') }}
+                                    </button>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
